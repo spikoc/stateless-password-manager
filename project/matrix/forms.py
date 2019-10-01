1c9db210e6c064ea3324d590e94ad41a4a2bff1d
Kostas Tzoulas <kostantinos.tzoulas@sgdigital.com>
2019-10-01 19:28:33 +0300

    Replace placeholders with description field

--- project/matrix/forms.py
+++ project/matrix/forms.py
@@ -8,12 +8,14 @@ from wtforms import BooleanField, IntegerField, StringField
 class EditForm(FlaskForm):
     """TODO: add class docstring"""
 
-    name = StringField('name')
-    algorithm = StringField('hash digest algorithm')
-    salt = StringField('salt')
-    iterations = IntegerField('number of iterations')
-    uppercase = BooleanField('uppercase letters')
-    lowercase = BooleanField('lowercase letters')
-    digits = BooleanField('digits')
-    symbols = BooleanField('symbols')
-    length = IntegerField('length of password')
+    name = StringField('Name', description='Name of the matrix')
+    algorithm = StringField('Algorithm', description='Select the hash digest algorithm')
+    salt = StringField('Salt', description='Salts make the search space larger in the case of '
+                                           'brute forcing')
+    uppercase = BooleanField('Contains uppercase letters')
+    lowercase = BooleanField('Contains lowercase letters')
+    digits = BooleanField('Contains digits')
+    symbols = BooleanField('Contains symbols')
+    iterations = IntegerField('Iterations', description='Number or iterations in calculation '
+                                                        '(higher means more computation required)')
+    length = IntegerField('Length', description='length of the output password (not required)')
