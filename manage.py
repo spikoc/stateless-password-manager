172cd4617e04499388608d7869457a00fa39b27d
Kostas Tzoulas <kostantinos.tzoulas@sgdigital.com>
2019-10-03 13:02:24 +0300

    Install coverage and modify `manage.py` method to support it

--- manage.py
+++ manage.py
@@ -4,12 +4,47 @@
 """
 # pylint: disable=invalid-name
 
+import sys
+import time
+import unittest
+
+from coverage import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app
 
 # app = create_app()
 cli = FlaskGroup(create_app=create_app)
+cov = coverage(branch=True, include='project/*', omit=[
+    'project/config.py',
+    'project/__init__.py',
+    'project/*/__init__.py'
+])
+cov.start()
+
+
+@cli.command()
+def run_coverage():
+    """Run the unit tests with coverage."""
+    try:
+        tests = unittest.TestLoader().discover('tests')
+        result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+        assert result.wasSuccessful()
+
+    except AssertionError:
+        sys.exit(1)
+
+    finally:
+        cov.stop()
+        cov.save()
+
+    time.sleep(1)  # ..................avoid collapse
+    print('Coverage Summary:')
+
+    cov.report()
+    cov.html_report()
+    cov.erase()
 
 
 if __name__ == '__main__':
--- project/config.py
+++ project/config.py
@@ -12,6 +12,7 @@ class BaseConfig:
     """Base Configuration"""
 
     APP_NAME = os.getenv('APP_NAME', 'stateless-password-manager')
+    DEBUG = False
     DEBUG_TB_ENABLED = False
     SECRET_KEY = os.getenv('SECRET_KEY', '77c84dc23ad11ebd1e78e80acf73ce8a')
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/spamandb'
