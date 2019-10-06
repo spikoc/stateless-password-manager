ee98a1816ca579d8b3c5e8e5f9e07f6015a0dcf0
Kostas Tzoulas <kostantinos.tzoulas@sgdigital.com>
2019-10-06 22:50:21 +0300

    Select hash digest algorithm from table

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
     TODO: add module docstring
 """
 # pylint: disable=invalid-name
+# pylint: disable=no-member
 
 import sys
 import time
 import unittest
+
+from datetime import datetime
 
 from coverage import coverage
 from flask.cli import FlaskGroup
@@ -21,6 +24,16 @@ cov = coverage(branch=True, include='project/*', omit=[
     'project/*/__init__.py'
 ])
 cov.start()
+
+
+def _commit():
+    """Commit record changes - rollback in case of error."""
+    try:
+        db.session.commit()
+
+    except Exception as err:
+        print(err.__class__.__name__, err)
+        db.session.rollback()
 
 
 def _execute_unittests():
@@ -39,8 +52,58 @@ def _execute_unittests():
         cov.save()
 
 
+def _generate_hash_algorithms():
+    """Generate hash algorithm data samples."""
+    from project.matrix.models import Algorithm
+
+    models = [
+        Algorithm(name='md5'),
+        Algorithm(name='sha1'),
+        Algorithm(name='sha224'),
+        Algorithm(name='sha256'),
+        Algorithm(name='sha384'),
+        Algorithm(name='sha512')
+    ]
+
+    for model in models:
+        print(model)
+        db.session.add(model)
+
+
+def _generate_matrices():
+    """Generate matrix data samples."""
+    from project.matrix.models import Matrix
+
+    models = [
+        Matrix(name='Gmail', algorithm_id=4, salt='fd70e5b3b577a1572436847b8e65875e',
+               created_at=datetime.now(), modified_at=datetime.now()),
+        Matrix(name='Facebook', algorithm_id=6, salt='60a8ec4be6ab4b909ea811a18156781c',
+               iterations=2048, created_at=datetime.now(), modified_at=datetime.now()),
+        Matrix(name='Instagram', algorithm_id=1, salt='a1791e97739c206a7bb09ddff3ecb51d',
+               iterations=512, length=8, created_at=datetime.now(), modified_at=datetime.now()),
+        Matrix(name='LinkedIn', algorithm_id=2, salt='0e9c6984dfc3ad02827f0c67288b0bc5',
+               iterations=4096, length=32, created_at=datetime.now(), modified_at=datetime.now()),
+        Matrix(name='Codecademy', algorithm_id=5, salt='a7c331d5c1a75b0894871da345ac05db',
+               iterations=4096, length=12, created_at=datetime.now(), modified_at=datetime.now()),
+        Matrix(name='Slack', algorithm_id=3, salt='5d7549459403b55798c3e713facfeea4',
+               iterations=128, created_at=datetime.now(), modified_at=datetime.now())
+    ]
+
+    for model in models:
+        print(model)
+        db.session.add(model)
+
+
 @cli.command()
-def create_db():
+def generate_sample_data():
+    """Feed the database with sample data."""
+    _generate_hash_algorithms()
+    _generate_matrices()
+    _commit()
+
+
+@cli.command()
+def init_db():
     """Clear the database and create a new schema."""
     print('[*] Drops all tables...')
     db.drop_all()
--- project/matrix/models.py
+++ project/matrix/models.py
@@ -4,6 +4,18 @@
 from project import db
 
 
+class Algorithm(db.Model):
+    """State the columns that describe a hash algorithm model."""
+
+    __tablename__ = 'algorithm'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(8), nullable=False)
+
+    def __repr__(self):
+        return "<{}(id={}, name='{}')>".format(self.__class__.__name__, self.id, self.name)
+
+
 class Matrix(db.Model):
     """State the columns that describe a matrix model."""
 
@@ -11,9 +23,14 @@ class Matrix(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, nullable=False)
-    algorithm = db.Column(db.String(16), nullable=False)
+    algorithm_id = db.Column(db.Integer, db.ForeignKey('algorithm.id'))
     salt = db.Column(db.String(32), nullable=False)
     iterations = db.Column(db.Integer, nullable=False, default=1024)
     length = db.Column(db.Integer, nullable=False, default=8)
-    created_at = db.Column(db.Date, nullable=False)
-    modified_at = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    modified_at = db.Column(db.DateTime, nullable=False)
+
+    algorithm = db.relationship('Algorithm', foreign_keys='Matrix.algorithm_id')
+
+    def __repr__(self):
+        return "<{}(id={}, name='{}')>".format(self.__class__.__name__, self.id, self.name)
