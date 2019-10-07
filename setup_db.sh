4288bb0a038154b77b94d78d6102e5090dc366a0
Kostas Tzoulas <kostantinos.tzoulas@sgdigital.com>
2019-10-07 14:53:39 +0300

    Set up docker database before run the application

---
+++ setup_db.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# .....................................start db docker
+docker run --rm --name pg-docker \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -p 5432:5432 postgres
+  # -v ~/docker/volumes/postgresql/data:/var/lib/postgresql/data
+
+# .....................................create database
+psql -h localhost -U postgres -c 'CREATE DATABASE spamandb;'
+
+# .....................................connect to spamandb
+# psql -h localhost -U postgres -d spamandb
