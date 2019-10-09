4e9722a21d70168f16c903e264b7615a72d6fff3
Kostas Tzoulas <kostantinos.tzoulas@sgdigital.com>
2019-10-09 15:43:45 +0300

    Modified form fields for adding matrices

--- project/matrix/forms.py
+++ project/matrix/forms.py
@@ -1,21 +1,36 @@
 """
-    TODO: add module docstring
+    Contains all the forms used in the blueprint.
 """
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, IntegerField, StringField
+from wtforms import BooleanField, IntegerField, SelectField, StringField
+
+from project.matrix.models import Algorithm
 
 
 class EditForm(FlaskForm):
-    """TODO: add class docstring"""
+    """Describe the form for adding/editing :class:`project.matrix.models.Matrix` objects."""
 
-    name = StringField('Name', description='Name of the matrix')
-    algorithm = StringField('Algorithm', description='Select the hash digest algorithm')
-    salt = StringField('Salt', description='Salts make the search space larger in the case of '
-                                           'brute forcing')
+    name = StringField('Name')
+    algorithm_id = SelectField(
+        'Hash Algorithm',
+        coerce=int,
+        description='select the hash algorithm to be used for encoding')
+    salt = StringField(
+        'Salt',
+        description='salts make the search space larger in the case of brute forcing')
     uppercase = BooleanField('Contains uppercase letters')
     lowercase = BooleanField('Contains lowercase letters')
     digits = BooleanField('Contains digits')
     symbols = BooleanField('Contains symbols')
-    iterations = IntegerField('Iterations', description='Number or iterations in calculation '
-                                                        '(higher means more computation required)')
-    length = IntegerField('Length', description='length of the output password (not required)')
+    iterations = IntegerField(
+        'Iterations',
+        description='iterations in calculation (higher means more computation required)')
+    length = IntegerField('Length')
+
+    def __init__(self, *args, **kwargs):
+        super(EditForm, self).__init__(*args, **kwargs)
+
+        # .............................specify the available options in the selection field
+        self.algorithm_id.choices = [(0, '-- select a hash algorithm --')]
+        self.algorithm_id.choices.extend(
+            [(x.id, x.name) for x in Algorithm.query.order_by(Algorithm.name).all()])
