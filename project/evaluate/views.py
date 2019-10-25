347743490425059c128ad103624026539c201482
spikoc <spikoc@hotmail.com>
2019-10-25 20:05:08 +0300

    Power message implementation

--- project/evaluate/views.py
+++ project/evaluate/views.py
@@ -11,9 +11,13 @@ evaluate_blueprint = Blueprint('evaluate', __name__, url_prefix='/evaluate')
 @evaluate_blueprint.route('/', methods=['GET', 'POST'])
 def index():
 
+    power = None
     form = EvaluateForm(request.form)
     if form.validate_on_submit():
-        print("ok")
+        print(form.password.data)
+        if form.password.data != None and form.password.data != '':
+            power = 'Weak Password'
 
-        return redirect(url_for('matrix.index'))
+        return render_template('evaluate/index.html', form=form, power=power)
+
     return render_template('evaluate/index.html', form=form)
